85cd1b5c81886bad0c25d37772dbd6ad557b6643
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-13 14:30:37 -0400

    agfdsg?

--- main.ts
+++ main.ts
@@ -9,7 +9,6 @@ let lightvalue: number = 0
 lightvalue = input.lightLevel()
 let neopixelStrip: neopixel.Strip = null
 
-
 // screen setup
 basic.clearScreen()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
@@ -21,39 +20,47 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  lightvalue = input.lightLevel()
+    lightvalue = input.lightLevel()
     if (lightvalue <= 51) {
-    }
-    if (lightvalue >= 52) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
-    if (lightvalue >= 104) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
-    if (lightvalue >= 156) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
+  }
+  if (lightvalue >= 52) {
+      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+  }
+  if (lightvalue >= 104) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+      neopixelStrip.show()
+      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+      neopixelStrip.show()
+  }
+  if (lightvalue >= 156) {
+      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+  }
 
     if (lightvalue >= 208) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+  }
 })
-input.onButtonPressed(Button.B, function () {}
-
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+  }
 )
