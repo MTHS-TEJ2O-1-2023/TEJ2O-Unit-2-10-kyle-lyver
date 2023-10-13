c1118f4392d3286391f62f71f717c8590de66efe
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-13 13:28:03 -0400

    fshje

--- main.ts
+++ main.ts
@@ -2,7 +2,58 @@
  *
  * Created by: Kyle Lyver
  * Created on: Oct 2023
- * This program ...
+ * This program makes neopixels light up according to the light level
 */
 
-basic.showString('Hello, World!')
+let lightvalue: number = 0
+lightvalue = input.lightLevel()
+let neopixelStrip: neopixel.Strip = null
+
+
+// screen setup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  lightvalue = input.lightLevel()
+    if (lightvalue <= 51) {
+    }
+    if (lightvalue >= 52) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+    }
+    if (lightvalue >= 104) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+    }
+    if (lightvalue >= 156) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+    }
+
+    if (lightvalue >= 208) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+    }
+})
+input.onButtonPressed(Button.B, function () {}
+
+)
