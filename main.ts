55e0d0ab2a3b3422af5beabb36971d88f9588f40
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-16 13:24:51 -0400

    chamge cp,m,emts

--- main.ts
+++ main.ts
@@ -5,12 +5,13 @@
  * This program makes neopixels light up according to the light level
 */
 
+// variables
 let lightvalue: number = 0
-lightvalue = input.lightLevel()
 let neopixelStrip: neopixel.Strip = null
 
 // screen setup
 basic.clearScreen()
+lightvalue = input.lightLevel()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -20,22 +21,32 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
+  // check if <= 51 light level
   lightvalue = input.lightLevel()
   if (lightvalue <= 51) {
+  basic.showNumber(0)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
   }
 
+  // check if >= 52 light level
   if (lightvalue >= 52) {
+  basic.showNumber(1)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
     neopixelStrip.show()
   }
+
+  // 
   if (lightvalue >= 104) {
+    basic.showNumber(2)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
     neopixelStrip.show()
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
     neopixelStrip.show()
   }
+
+  // 
   if (lightvalue >= 156) {
+    basic.showNumber(3)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
     neopixelStrip.show()
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
@@ -44,7 +55,9 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
   }
 
+  //
   if (lightvalue >= 208) {
+    basic.showNumber(4)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
     neopixelStrip.show()
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
@@ -55,6 +68,8 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
   }
 })
+
+// 
 input.onButtonPressed(Button.B, function () {
   basic.clearScreen()
   neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
@@ -64,5 +79,4 @@ input.onButtonPressed(Button.B, function () {
   neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.show()
   basic.showIcon(IconNames.Happy)
-}
-)
+})
