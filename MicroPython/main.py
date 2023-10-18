b50a242a83b80c6c6a088b981f706a054170311b
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-18 16:27:42 +0000

    gyoy?

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,19 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
+num_pixels = 12
+np = neopixel.NeoPixel(pin16, 4)
+light_Level = number = 0
 
-display.scroll("Hello, World!")
+while True:
+    display.clear()
+    np[0] = (0, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
+    np.show()
+    display.show(Image.HAPPY)
+    if button_a.is_pressed():
+        if input.
