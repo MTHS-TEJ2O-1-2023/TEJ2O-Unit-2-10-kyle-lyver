dbccf91211cf898f7f647f2a52db8796a36acaf6
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-20 16:59:43 +0000

    better pie

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,7 +9,7 @@ import neopixel
 
 num_pixels = 12
 np = neopixel.NeoPixel(pin16, 4)
-light_Level = input.light_level()
+amount_of_light = 0
 
 while True:
     display.clear()
@@ -20,26 +20,26 @@ while True:
     np.show()
     display.show(Image.HAPPY)
     if button_a.is_pressed():
-        if light_Level <= 51:
+        if amount_of_light <= 51:
             np[0] = (0, 0, 0)
             np.show()
             display.scroll("0")
-        if light_Level >= 52:
+        if amount_of_light >= 52:
             np[0] = (128, 0, 128)
             np.show()
             display.scroll("1")
-        if light_Level >= 104:
+        if amount_of_light >= 104:
             np[0] = (128, 0, 128)
             np[1] = (128, 0, 128)
             np.show()
             display.scroll("2")
-        if light_Level >= 156:
+        if amount_of_light >= 156:
             np[0] = (128, 0, 128)
             np[1] = (128, 0, 128)
             np[2] = (128, 0, 128)
             np.show()
             display.scroll("3")
-        if light_Level >= 204:
+        if amount_of_light >= 204:
             np[0] = (128, 0, 128)
             np[1] = (128, 0, 128)
             np[2] = (128, 0, 128)
